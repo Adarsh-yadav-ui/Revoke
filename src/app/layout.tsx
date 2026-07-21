2f5a70853853224320f0899dd702638668e89853
Adarsh Yadav <akankshaazm20@gmail.com>
2026-07-21 20:52:35 +0530

    feat: configured clerk for auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,10 @@ import { ThemeProvider } from "@/providers/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 
+import { shadcn } from "@clerk/ui/themes"
+import { ClerkProvider, Show, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
+
+
 const manrope = Manrope({
   subsets: ["latin"],
   weight: "400",
@@ -51,7 +55,22 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col">
         <ThemeProvider>
           <TooltipProvider>
-            {children}
+            <ClerkProvider appearance={{ theme: shadcn }}>
+              <header className="flex justify-end items-center p-4 gap-4 h-16">
+                <Show when="signed-out">
+                  <SignInButton />
+                  <SignUpButton>
+                    <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                      Sign Up
+                    </button>
+                  </SignUpButton>
+                </Show>
+                <Show when="signed-in">
+                  <UserButton />
+                </Show>
+              </header>
+              {children}
+            </ClerkProvider>
           </TooltipProvider>
           <Toaster />
         </ThemeProvider>
